fecc83e66d281305502017b5d8e50b857252943d
Gabriela Santos <gabrielasantos@gabdev.local>
2022-07-05 21:11:24 -0300

    Implements scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_studies/widgets/basic_widgets/scaffold_widget.dart';
+import 'package:flutter_studies/widgets/basic_widgets/scaffold/scaffold_widget.dart';
 
 void main() {
   runApp(const MyApp());
